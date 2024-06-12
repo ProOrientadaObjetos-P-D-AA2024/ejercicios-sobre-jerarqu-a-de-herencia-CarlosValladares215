cc8ab4593d74ef3b3556f9d3ef0d631f5aed84c0
Carlos Valladares <csvalladares2@utpl.edu.ec>
2024-06-12 12:55:35 -0500

    Trabajo

---
+++ Modelos_UML/Problema5_VentaEntradasTeatro/src/problema5_ventaentradasteatro/Problema5_VentaEntradasTeatro.java
@@ -0,0 +1,17 @@
+
+package problema5_ventaentradasteatro;
+
+/**
+ *
+ * @author vcarl
+ */
+public class Problema5_VentaEntradasTeatro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
